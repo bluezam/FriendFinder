9c6c135750b59e8aecfc3466a4cd21ead79a098a
Michael <michaelbao13@gmail>
2019-04-25 04:47:55 -0700

    test

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Pull in required dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// Configure the Express application
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT;
 
 // Expose the public directory to access CSS files
 app.use(express.static(path.join(__dirname, './app/public')));
